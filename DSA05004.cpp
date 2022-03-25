5ad8207cba328e093fafc4dc079db0e8252c8b18
VuNAP <vuvovan2k2>
2022-03-25 10:18:05 +0700

    DSA05004 DAY CON TANG DAI NHAT

---
+++ DSA05004.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+
+using namespace std;
+
+#define endl "\n"
+#define faster() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n+2);
+    for(int i = 1; i <= n; i ++) cin >> a[i];
+    a[0] = -1e9;
+    a[n+1] = 1e9;
+    int imax = n+1;
+    vector<int> l(n+2,1);
+    int res = 0;
+    for(int i = 1; i <= n; i ++) {
+        for(int j = 1; j < i; j ++) {
+            if(a[j] < a[i]) l[i] = max(l[i], l[j] + 1);
+            res = max(res,l[i]);
+        }
+    }
+    cout << res;
+}
+
+int main()
+{
+    faster();
+    solve();
+}
