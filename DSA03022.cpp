ef0af96ae1238220a6e5c7cc659cc3705bfb5c4e
VuNAP <vuvovan2k2>
2022-04-05 10:37:08 +0700

    DSA03022 TICH LON NHAT

--- DSA03022.cpp
+++ DSA03022.cpp
@@ -7,35 +7,16 @@ using namespace std;
 
 void solve()
 {
-    bool check = false;
-    int n , m , k;
-    cin >> n >> m >> k;
-    vector<int> a(n), b(m), c(k);
-    for(int i = 0 ; i < n ; i ++) cin >> a[i];
-    for(int i = 0 ; i < m ; i ++) cin >> b[i];
-    for(int i = 0 ; i < k ; i ++) cin >> c[i];
-    int x = 0, y = 0, z = 0;
-    while(x < n && y < m && z < k) {
-        if(a[x] < b[y] || a[x] < c[z])x ++;
-        else if(b[y] < a[x] || b[y] < c[z]) y ++;
-        else if(c[z] < a[x] || c[z] < b[y]) z ++;
-        else if(a[x] == b[y] && b[y]  == c[z]) {
-            cout << a[x] << " ";
-            check = true;
-            x ++; y ++; z ++;
-        }
-    }
-    if(!check) cout << "NO";
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n ; i ++) cin >> a[i];
+    sort(a.begin(), a.end());
+    cout << max(a[0] * a[1], max(a[0] * a[1] * a[n - 1], max(a[n - 1] * a[n - 2], a[n - 1] * a[n - 2] * a[n - 3])));
 }
 
 int main()
 {
     faster();
-    int t;
-    cin >> t;
-    while(t--)
-    {
-        solve();
-        cout << endl;
-    }
+    solve();
 }
